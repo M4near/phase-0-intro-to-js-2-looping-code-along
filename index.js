c4c482b3b6fbd073de4febaf6736ae036a48eae6
Michael Fournier <michaeljfournier5@gmail.com>
2022-02-25 17:24:39 -0500

    commit tree 4c24f500fb0d2b1e80efc8b5ab79ad4f0e2082e0

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+
+const event = ["Surprise"]
+
+function writeCards(names, event) {
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push (`Thank you, ${names [i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(number){
+    let countdown = number;
+    while (countdown >= 0){
+        console.log(countdown--);
+    }
+}
+countDown(10);
